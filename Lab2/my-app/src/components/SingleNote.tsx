bac2854d77f954e82d1dc54a0724ca1700999d1f
datungLA <97591324+datungLA@users.noreply.github.com>
2024-10-11 12:34:12 -0700

    Favorite button done

--- Lab2/my-app/src/components/SingleNote.tsx
+++ Lab2/my-app/src/components/SingleNote.tsx
@@ -7,10 +7,19 @@ type Props = {
     setNotes: React.Dispatch<React.SetStateAction<Note[]>>
 }
 const SingleNote = ({ note, notes, setNotes }: Props) => {
+    const handleFavorite = (id: number) => () => {
+        setNotes(notes.map(note => note.id == id ? { ...note, isLiked: !note.isLiked } : note))
+    }
+    const handleDelete = (id: number) => () => {
+        setNotes(notes.filter(note => note.id !== id))
+    }
     return (
         <div key={note.id} className='note-item'>
             <div className="notes-header">
-                <span>
+                <span onClick={handleFavorite(note.id)}>
+                    {note.isLiked ? '❤️' : '♡'}
+                </span>
+                <span onClick={handleDelete(note.id)}>
                     <button>x</button>
                 </span>
             </div>

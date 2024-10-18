54eb82520efbd9ec5098ec0c0b09f9bbcab90442
datungLA <97591324+datungLA@users.noreply.github.com>
2024-10-18 13:34:34 -0700

    Tests for toDoList

--- Lab3/my-app/src/tests/toDoList.test.tsx
+++ Lab3/my-app/src/tests/toDoList.test.tsx
@@ -1,13 +1,31 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { ToDoList } from "../toDoList";
-import { Label } from "../components/types";
+import { dummyGroceryList } from "../components/constants";
 import '@testing-library/jest-dom';
 
 describe("Test ToDoList component", () => {
     it("renders ToDoList display", () => {
-        /* Are all the items in the list displayed on the screen */
+        const mockGroceryList = dummyGroceryList;
         render(<ToDoList />);
-        const listItems = screen.getAllByRole("listitem");
-        expect(listItems.length).toBeGreaterThan(0);
+        const itemBoughtTitle = screen.getByText("Items bought: 0");
+        const appleTitle = screen.getByText(mockGroceryList[0].name);
+        const bananaTitle = screen.getByText(mockGroceryList[1].name)
+        expect(appleTitle).toBeInTheDocument();
+        expect(bananaTitle).toBeInTheDocument();
+        expect(itemBoughtTitle).toBeInTheDocument();
+    })
+    it("click and unclick on items' checkbox", () => {
+        render(<ToDoList />)
+        const items = screen.getAllByRole("checkbox");
+        fireEvent.click(items[0]);
+        fireEvent.click(items[0]);
+        let itemBoughtTitle = screen.getByText("Items bought: 2")
+        expect(itemBoughtTitle).toBeInTheDocument();
+        fireEvent.click(items[0]);
+        itemBoughtTitle = screen.getByText("Items bought: 1")
+        expect(itemBoughtTitle).toBeInTheDocument();
+        fireEvent.click(items[1]);
+        itemBoughtTitle = screen.getByText("Items bought: 0")
+        expect(itemBoughtTitle).toBeInTheDocument();
     })
 })
